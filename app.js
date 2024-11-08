faaf7265af5cae7e3be286793613e88a8ce08739
Nabeel Mansour <mikaeel1@hotmail.com>
2024-11-08 11:02:49 +0100

    modified kontakt.html and added app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const nav = document.getElementById("nav");
+const navClose = document.getElementById("nav-close");
+
+// show nav
+navBtn.addEventListener("click", () => {
+  nav.classList.add("showNav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+  nav.classList.remove("showNav");
+});
